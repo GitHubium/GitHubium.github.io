129c060acd34a15dc82b711918d90f1236f30b16
Squishy <squishycaterpillar@gmail.com>
2019-10-02 13:01:25 -0400

    Idk what I changed

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-function submit() {
-  console.log(document);
-  var num1 = Number(document.getElementById("n1").value);
-  var num2 = Number(document.getElementById("n2").value);
-  var operator = document.getElementById("chooseOper").value;
-  switch(operator) {
-    case "add":
-      ans = num1+num2;
-      break;
-    case "minus":
-      ans = num1-num2;
-      break;
-    case "multiply":
-      ans = num1*num2;
-      break;
-    case "divide":
-      ans = num1/num2;
-      break;
+
+var nameDivHeight = 0;
+
+var isNameDivVisible = false;
+var name;
+
+var e = Object.create({});
+
+function generateName() {
+  isNameDivVisible = true;
+  e.name_div.style.display = "block";
+  e.name_canvas.style.width = windowWidth;
+  name = e.name_input.value;
+}
+
+function update() {
+  if (isNameDivVisible) {
+    nameDivHeight += (500-nameDivHeight)*0.1;
+    e.name_div.style.height = nameDivHeight+"px";
+    e.name_canvas.style.height = nameDivHeight-150;
   }
-  document.getElementById("result").innerHTML = "Answer: "+ans
+
+
+}
+
+function metaUpdate() {
+  update();
+  window.requestAnimationFrame(metaUpdate);
+}
+
+function pageLoaded() {
+  // init stuff
+  var elements = ["name_div", "name_input", "name_canvas"]
+  for (var i = 0; i < elements.length; i ++) {
+    var ele = elements[i];
+    e[ele] = document.getElementById(ele);
+  }
+
+
+  window.requestAnimationFrame(metaUpdate);
 }
